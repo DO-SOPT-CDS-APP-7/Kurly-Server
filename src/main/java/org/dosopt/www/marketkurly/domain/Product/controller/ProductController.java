e5f46618d66f28913186e496bfe6925dd913db0a
dahyunkim <dahyun_ee@naver.com>
2023-11-16 17:49:56 +0900

    [Feature] Swagger 예시 작성

--- src/main/java/org/dosopt/www/marketkurly/domain/Product/controller/ProductController.java
+++ src/main/java/org/dosopt/www/marketkurly/domain/Product/controller/ProductController.java
@@ -1,9 +1,23 @@
 package org.dosopt.www.marketkurly.domain.Product.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.dosopt.www.marketkurly.global.result.ResultResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "PRODUCT cotroller", description = "상품 API입니다.")
 @RestController
 public class ProductController {
+   @Operation( summary = "특정 상품 조회",
+         description = "사용자가 특정 상품을 조회합니다.",
+         tags = "PRODUCT",
+         parameters = { @Parameter(name = "productId", description = "상품 ID", in = ParameterIn.PATH) }
+   )
+   @GetMapping("/{productId}")
+   public void getProductById(@PathVariable("productId") Long id) {
+   }
 }
