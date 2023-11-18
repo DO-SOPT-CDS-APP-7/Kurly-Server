9701d001c0412537cd23d22e60946f6c0134ea0e
dahyunkim <dahyun_ee@naver.com>
2023-11-18 16:55:18 +0900

    [Feature] querydsl 설정 추가

--- src/main/java/org/dosopt/www/marketkurly/domain/Product/controller/ProductController.java
+++ src/main/java/org/dosopt/www/marketkurly/domain/Product/controller/ProductController.java
@@ -5,6 +5,8 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.dosopt.www.marketkurly.global.result.ResultResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "PRODUCT cotroller", description = "상품 API입니다.")
 @RestController
 public class ProductController {
+   private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
    @Operation( summary = "특정 상품 조회",
          description = "사용자가 특정 상품을 조회합니다.",
          tags = "PRODUCT",
@@ -19,5 +23,10 @@ public class ProductController {
    )
    @GetMapping("/{productId}")
    public void getProductById(@PathVariable("productId") Long id) {
+      logger.trace("Trace Level 테스트");
+      logger.debug("DEBUG Level 테스트");
+      logger.info("INFO Level 테스트");
+      logger.warn("Warn Level 테스트");
+      logger.error("ERROR Level 테스트");
    }
 }
